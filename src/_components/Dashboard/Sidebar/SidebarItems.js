34f6441dca4ad3b7a1c1638e2828ac6c1a1ecdfc
eldhose.saji <eldhose.saji@zohocorp.com>
2024-09-29 18:08:16 +0530

    close sidebar on click and removed unused menu items

--- src/_components/Dashboard/Sidebar/SidebarItems.js
+++ src/_components/Dashboard/Sidebar/SidebarItems.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import { useTemplateProvider } from "../Template/Template";
 
 const SidebarItems = (props) => {
     const { item = {}, isActive=false } = props;
+    const { toggleSidebar } = useTemplateProvider()
 
     const { id, text, route, svg } = item;
 
     return (
         <Link to={route}>
-            <div key={id} className={`flex py-2 px-3 text-sm rounded-md ${isActive ? 'bg-highlight' : 'hover-secondary'}`}>
+            <div key={id} onClick={toggleSidebar} className={`flex py-2 px-3 text-sm rounded-md ${isActive ? 'bg-highlight' : 'hover-secondary'}`}>
                 <span className="">
                     {svg}
                 </span>
