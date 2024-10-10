71404cbf9cd89a6ce5a67d7bb284b38fe6458ffd
eldhose.saji <eldhose.saji@zohocorp.com>
2024-10-10 23:38:43 +0530

    sidebar items added for later

--- src/_constants/SidebarValues.js
+++ src/_constants/SidebarValues.js
@@ -9,6 +9,27 @@ export const MENU_ITEMS = {
         },
         {
             id: 2,
+            text: 'Notes',
+            route: '/user/notes',
+            path: '/user/notes',
+            svg: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-notebook-pen"><path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4"/><path d="M2 6h4"/><path d="M2 10h4"/><path d="M2 14h4"/><path d="M2 18h4"/><path d="M21.378 5.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/></svg>
+        },
+        {
+            id: 3,
+            text: 'Tasks',
+            route: '/user/tasks',
+            path: '/user/tasks',
+            svg: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-checks"><path d="m3 17 2 2 4-4"/><path d="m3 7 2 2 4-4"/><path d="M13 6h8"/><path d="M13 12h8"/><path d="M13 18h8"/></svg>
+        },
+        {
+            id: 4,
+            text: 'Posts',
+            route: '/user/posts',
+            path: '/user/posts',
+            svg: <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rss"><path d="M4 11a9 9 0 0 1 9 9"/><path d="M4 4a16 16 0 0 1 16 16"/><circle cx="5" cy="19" r="1"/></svg>
+        },
+        {
+            id: 5,
             text: 'Publish',
             route: '/user/publish',
             path: '/user/publish',
