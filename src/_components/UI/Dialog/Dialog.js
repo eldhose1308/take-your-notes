7a0f13dcfa4a10c9bdaa9b09f5438566dd1d33ce
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-09 00:58:37 +0530

    dialog size option added

--- src/_components/UI/Dialog/Dialog.js
+++ src/_components/UI/Dialog/Dialog.js
@@ -7,7 +7,7 @@ import './Dialog.css'
 
 // create a key so that the toast re-mounts after every call
 const Dialog = ({ children, ...props }) => {
-    const { isShown = false, hasOverlay=false, type: defaultType = 'default', options = {}, message = {}, progress } = props;
+    const { isShown = false, hasOverlay=false, size='md', type: defaultType = 'default', options = {}, message = {}, progress } = props;
     const { heading = 'Your request is processing', description = 'Please wait for some moment.' } = message;
     const { position = 'top-right', } = options;
 
@@ -24,7 +24,7 @@ const Dialog = ({ children, ...props }) => {
             {/* {children} */}
             {/* <div className={`dialog dialog-${positionX} dialog-${positionY} z-50 fixed flex items-center justify-end animate-slide-in-y`}> */}
             <div className={`dialog dialog-centers w-full dialog-top z-50 fixed flex items-center justify-center animate-slide-in-y`}>
-                <div className="min-w-sm dialog-md">
+                <div className={`min-w-sm dialog-${size}`}>
                 {/* <div className="max-w-sm min-w-sm"> */}
                     <div className={`card bg-default rounded-lg m-3`}>
 
