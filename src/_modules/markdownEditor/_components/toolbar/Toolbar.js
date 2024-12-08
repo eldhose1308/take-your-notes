1ea4ef852e7c3952ce52da4c121832b21b41a5bb
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-09 00:48:16 +0530

    toolbar alignmnt

--- src/_modules/markdownEditor/_components/toolbar/Toolbar.js
+++ src/_modules/markdownEditor/_components/toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import EditorModeSelector from "./EditorModeSelector";
 import ToolbarButton from "./ToolbarButton";
 
 const Toolbar = (props) => {
-    const { textFormats=[], activeTextFormat={}, onEditorModeChange = () => { }, onImageInsert = () => { }, onTextFormat = () => { } } = props;
+    const { textFormats = [], activeTextFormat = {}, onEditorModeChange = () => { }, onImageInsert = () => { }, onTextFormat = () => { } } = props;
 
     const [mode, setMode] = useState(TOOLBAR_MODES.EDIT_PREVIEW)
     // const [activeFormat, setActiveFormat] = useState(null);
@@ -20,23 +20,26 @@ const Toolbar = (props) => {
     }
 
     return (
-        <div className="flex border border-custom rounded-md px-2 py-1 m-2">
+        <div className="flex justify-between border border-custom rounded-md px-2 py-1 m-2">
             <EditorModeSelector mode={mode} onChange={handleModeChange} />
 
 
-            {textFormats.map((format) => {
-                const { title, value, icon } = format;
-                const isActive = activeTextFormat[value];
-                return (
-                    <ToolbarButton key={`Toolbar_btn_${value}`} isActive={isActive} title={title} value={value} onClick={handleTextFormatting}>
-                        {icon}
-                    </ToolbarButton>
-                )
-            })}
+            <div className="flex">
 
-            <ToolbarButton title='Insert File' value='insertFile' onClick={onImageInsert}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-up"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="M12 12v6" /><path d="m15 15-3-3-3 3" /></svg>
-            </ToolbarButton>
+                {textFormats.map((format) => {
+                    const { title, value, icon } = format;
+                    const isActive = activeTextFormat[value];
+                    return (
+                        <ToolbarButton key={`Toolbar_btn_${value}`} isActive={isActive} title={title} value={value} onClick={handleTextFormatting}>
+                            {icon}
+                        </ToolbarButton>
+                    )
+                })}
+
+                <ToolbarButton title='Insert File' value='insertFile' onClick={onImageInsert}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-up"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><path d="M12 12v6" /><path d="m15 15-3-3-3 3" /></svg>
+                </ToolbarButton>
+            </div>
 
 
         </div>
