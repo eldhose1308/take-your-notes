24bc717f5e9ae4242e046c2727e6e794315ddca7
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-09 01:53:21 +0530

    keyboard shortcut icon shange

--- src/_modules/markdownEditor/_components/toolbar/Toolbar.js
+++ src/_modules/markdownEditor/_components/toolbar/Toolbar.js
@@ -41,7 +41,7 @@ const Toolbar = (props) => {
                 </ToolbarButton>
 
                 <ToolbarButton title='Info' value='info' onClick={onHelpOpen}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-help"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
                 </ToolbarButton>
 
 
