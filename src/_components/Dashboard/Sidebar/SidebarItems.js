0f50b1250f884117c27afe0bd8838808b289cd7e
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-12 23:01:06 +0530

    lock symbol removed on auth routes

--- src/_components/Dashboard/Sidebar/SidebarItems.js
+++ src/_components/Dashboard/Sidebar/SidebarItems.js
@@ -22,9 +22,9 @@ const SidebarItems = (props) => {
                 <span className="mx-2">
                     {text}
                 </span>
-                {needsAuth && <span>
+                {/* {needsAuth && <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-lock-keyhole"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg>
-                </span>}
+                </span>} */}
             </div>
         </Link>
     )
