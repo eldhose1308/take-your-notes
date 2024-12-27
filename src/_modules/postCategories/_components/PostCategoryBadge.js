c19d031edf881a18f4ec0b5675203e65740fdf4d
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-28 00:26:36 +0530

    post category badge hover color change

--- src/_modules/postCategories/_components/PostCategoryBadge.js
+++ src/_modules/postCategories/_components/PostCategoryBadge.js
@@ -9,7 +9,7 @@ const PostCategoryBadge = (props) => {
     const categoryDetailRoute = CLIENT_ROUTES.CATEGORY_DETAIL(categorySlug);
 
     return (
-        <Link to={categoryDetailRoute} className='bg-custom hover-another border-2 border-custom rounded-md flex cursor-pointer px-2 py-1.5 mt-2'>
+        <Link to={categoryDetailRoute} className='bg-custom hover-default border-2 border-custom rounded-md flex cursor-pointer px-2 py-1.5 mt-2'>
             <div className="flex items-center">
                 <span className="flex items-center mr-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-square-menu"><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M7 8h10" /><path d="M7 12h10" /><path d="M7 16h10" /></svg>
