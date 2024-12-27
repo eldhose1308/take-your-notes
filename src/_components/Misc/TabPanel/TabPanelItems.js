31c94d6b829c95326e3a9f74284716a0a9c9344a
eldhose.saji <eldhose.saji@zohocorp.com>
2024-12-28 00:22:07 +0530

    splitted tabITem to small compoennt

--- src/_components/Misc/TabPanel/TabPanelItems.js
+++ src/_components/Misc/TabPanel/TabPanelItems.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const TabItem = ({ tabItem, isActive, onClick }) => {
+    const { id, label, icon } = tabItem;
+
+    return (
+        <div key={id} onClick={() => onClick(id)} className={`flex  items-center text-xs cursor-pointer rounded-md px-2 py-2 mx-1 ${isActive ? 'bg-accent text-custom' : 'bg-default hover-highlight'}`}>
+            <span className="flex items-center mr-2">{icon}</span>
+            <span>{label}</span>
+        </div>
+    )
+}
+
+
 const TabPanelItems = (props) => {
     const { tabItems, selectedTab, setSelectedTab } = props;
 
@@ -7,14 +19,9 @@ const TabPanelItems = (props) => {
         <div className="flex w-full border border-custom rounded-md bg-default px-1 py-2 text-sm">
 
             {tabItems.map((tabItem) => {
-                const { id, label, icon } = tabItem;
+                const { id } = tabItem;
                 const isActive = selectedTab === id;
-                return (
-                    <div key={id} onClick={() => setSelectedTab(id)} className={`flex  items-center text-xs cursor-pointer rounded-md px-2 py-2 mx-1 ${isActive ? 'bg-accent text-custom' : 'bg-default hover-highlight'}`}>
-                        <span className="flex items-center mr-2">{icon}</span>
-                        <span>{label}</span>
-                    </div>
-                )
+                return <TabItem key={id} tabItem={tabItem} isActive={isActive} onClick={setSelectedTab} />
             })}
         </div>
     )
