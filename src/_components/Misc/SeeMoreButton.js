7cf0638d67d94a7540f1c429d1ed660f5e0403f5
eldhose.saji <eldhose.saji@zohocorp.com>
2025-01-01 01:42:34 +0530

    seemorebtn click area increase

--- src/_components/Misc/SeeMoreButton.js
+++ src/_components/Misc/SeeMoreButton.js
@@ -5,13 +5,11 @@ const SeeMoreButton = (props) => {
     const { linkUrl, text='See More ...' } = props;
 
     return (
-        <div className='text-xs py-2 px-2 bg-transparent text-default border border-secondary hover-custom hover-text-accent flex justify-center rounded-md w-full cursor-pointer'>
-            <Link to={linkUrl} className="flex w-full items-center justify-center">
+            <Link to={linkUrl} className="text-xs py-2 px-2 bg-transparent text-default border border-secondary hover-custom hover-text-accent flex justify-center rounded-md w-full cursor-pointer">
                 <span className=''>
                     {text}
                 </span>
             </Link>
-        </div>
     )
 }
 
