e3d6e02b50cc1c547a7ce841e2086439237d43a0
eldhose.saji <eldhose.saji@zohocorp.com>
2025-01-07 23:28:05 +0530

    lastClickedPostItem in single call

--- src/_modules/posts/_components/list/PostList.js
+++ src/_modules/posts/_components/list/PostList.js
@@ -7,12 +7,12 @@ const PostList = (props) => {
     const { posts = [] } = props;
     const { getLastClickedPost } = usePostsCache();
 
-
+    const lastClickedPost = getLastClickedPost();
 
     return (
         <React.Fragment>
             <div className="flex w-full">
-                {posts.map(postItem => <PostListItem key={postItem.id} postItem={postItem} lastClickedPost={getLastClickedPost()} />)}
+                {posts.map(postItem => <PostListItem key={postItem.id} postItem={postItem} lastClickedPost={lastClickedPost} />)}
             </div>
         </React.Fragment>
     )
