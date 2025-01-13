c6af5ff66c8d784a2dc4892cafa851fd067103ca
eldhose.saji <eldhose.saji@zohocorp.com>
2025-01-14 01:44:44 +0530

    hide logout if user is not present

--- src/_modules/drawers/SettingsDrawer.js
+++ src/_modules/drawers/SettingsDrawer.js
@@ -13,12 +13,14 @@ import useUser from "_hooks/useUser";
 import { Link } from "react-router-dom";
 import CLIENT_ROUTES from "_routes/clientRoutes";
 import { useToast } from "_contexts/ToastProvider";
+import { getUserDetailsOfCurrentUser } from "_utils/userAuth";
 
 const SettingsDrawer = () => {
     const dispatch = useDispatch();
     const { logout } = useAuth();
     const { getUserPreferences } = useUser();
     const { fontMode: currentFont } = getUserPreferences();
+    const { userName } = getUserDetailsOfCurrentUser();
 
     const status = useSelector(state => state.drawers.settingsDrawer);
     const { toast } = useToast();
@@ -139,14 +141,14 @@ const SettingsDrawer = () => {
                 <Separator variant='another' className='w-full' />
 
                 <div className="flex my-3 w-full">
-                    <div onClick={logout} className="flex justify-between w-full items-center border-another hover-custom text-default hover-text-destructive px-2 py-2 mx-1 rounded-md cursor-pointer">
+                    {!!userName && <div onClick={logout} className="flex justify-between w-full items-center border-another hover-custom text-default hover-text-destructive px-2 py-2 mx-1 rounded-md cursor-pointer">
                         <span>
                             Logout
                         </span>
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M5 11H13V13H5V16L0 12L5 8V11ZM3.99927 18H6.70835C8.11862 19.2447 9.97111 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C9.97111 4 8.11862 4.75527 6.70835 6H3.99927C5.82368 3.57111 8.72836 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C8.72836 22 5.82368 20.4289 3.99927 18Z"></path></svg>
                         </span>
-                    </div>
+                    </div>}
                 </div>
 
 
